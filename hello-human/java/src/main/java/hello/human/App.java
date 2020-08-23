c08fe97b009f5fac2311219d41f141e689784163
Igor Khotin <khotin@gmx.com>
2020-08-23 19:00:15 +0300

    [+] hello-human in java

---
+++ hello-human/java/src/main/java/hello/human/App.java
@@ -0,0 +1,8 @@
+package hello.human;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Human!");
+        System.out.println("We come in peace.!");
+    }
+}
